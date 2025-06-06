b1cade26afc5bfb081815fa5414d056eff2e15fd
CHIWHI, FNEG <55752357+chiwei82@users.noreply.github.com>
2025-06-06 10:25:28 +0800

    Update vite.config.js

--- p_w/vite.config.js
+++ p_w/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/personal_website/',
   plugins: [
     react(),
-    tailwindcss(),],
+    tailwindcss(),
+    glsl({
+      include: ['**/*.glsl', '**/*.vert', '**/*.frag'], // 允許這些副檔名被載入為模組
+    }),],
 })
