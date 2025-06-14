ea6ded82a41f08382ff81223fda06db889db219b
CHIWHI, FNEG <55752357+chiwei82@users.noreply.github.com>
2025-06-14 16:21:33 +0800

    Update headerBox.jsx

--- p_w/src/components/three/objects/headerBox.jsx
+++ p_w/src/components/three/objects/headerBox.jsx
@@ -1,25 +1,73 @@
-import { useRef } from 'react'
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
+import { useEffect, useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { Box, OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
 
-export default function HeaderBox() {
-  const boxRef = useRef()
+const torusGeometry = new THREE.IcosahedronGeometry()
+const material = new THREE.MeshMatcapMaterial()
 
-  useFrame((state, delta) => {
-    if (boxRef.current) {
-      boxRef.current.rotation.y += delta * 1.5
-      boxRef.current.rotation.x -= delta * 1.5
-    }
-  })
+export default function HeaderBox()
+{
+    const donuts = useRef([])
 
-  return (
-    <>
-      <ambientLight intensity={1} />
-      <directionalLight position={[3, 3, 3]} />
-      <Box ref={boxRef} args={[2, 2, 2]} position={[0, -1, 0]}>
-        <meshStandardMaterial color="white" />
-      </Box>
-      <OrbitControls enableZoom={false} />
+    const [ matcapTexture ] = useMatcapTexture('C47004_F9D00C_EDAF04_E09704', 256)
+
+    useFrame((state, delta) =>
+    {
+        for(const donut of donuts.current)
+        {
+            donut.rotation.y += delta * 2
+        }
+    })
+
+    useEffect(() =>
+    {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    return <>
+        <OrbitControls makeDefault />
+
+        <Center>
+            <Text3D
+                material={ material }
+                font="./PSR.json"
+                size={ 2 }
+                height={ 0.5 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+            >
+                HELLO WORLD
+            </Text3D>
+        </Center>
+
+        { [...Array(100)].map((value, index) =>
+            <mesh
+                ref={ (element) => donuts.current[index] = element }
+                key={ index }
+                geometry={ torusGeometry }
+                material={ material }
+                position={ [
+                    (Math.random() - 0.5) * 50,
+                    (Math.random() - 0.5) * 25,
+                    (Math.random() - 0.5) * 50 - 15
+                ] }
+                scale={ 0.5 + Math.random() * 0.2 }
+                rotation={ [
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ] }
+            />
+        ) }
+
     </>
-  )
 }
