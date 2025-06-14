3b2049e5d7f58c22a99fa599bdd41df954551887
CHIWHI, FNEG <55752357+chiwei82@users.noreply.github.com>
2025-06-14 16:21:29 +0800

    Update ResumeSection.jsx

--- p_w/src/components/ResumeSection.jsx
+++ p_w/src/components/ResumeSection.jsx
@@ -10,34 +10,34 @@ export default function ResumeSection({ children, id }) {
 
   const sectionRef = useRef()
 
-  useEffect(() => {
-    const el = sectionRef.current
+  // useEffect(() => {
+  //   const el = sectionRef.current
 
-    const anim = gsap.fromTo(
-      el,
-      { opacity: 0, y: 50 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 0.6,
-        ease: 'power2.out',
-        paused: true,
-      }
-    )
+  //   const anim = gsap.fromTo(
+  //     el,
+  //     { opacity: 0, y: 50 },
+  //     {
+  //       opacity: 1,
+  //       y: 0,
+  //       duration: 0.6,
+  //       ease: 'power2.out',
+  //       paused: true,
+  //     }
+  //   )
 
-    ScrollTrigger.create({
-      trigger: el,
-      start: 'top 95%',
-      end: 'top 30%', // 可以根據需要調整
-      onEnter: () => anim.play(),
-      onLeaveBack: () => anim.reverse(), // 離開回上方時反向動畫
-    })
+  //   ScrollTrigger.create({
+  //     trigger: el,
+  //     start: 'top 95%',
+  //     end: 'top 30%', // 可以根據需要調整
+  //     onEnter: () => anim.play(),
+  //     onLeaveBack: () => anim.reverse(), // 離開回上方時反向動畫
+  //   })
 
-    return () => {
-      anim.kill()
-      ScrollTrigger.getAll().forEach(t => t.kill())
-    }
-  }, [])
+  //   return () => {
+  //     anim.kill()
+  //     ScrollTrigger.getAll().forEach(t => t.kill())
+  //   }
+  // }, [])
 
   return (
     <section id={id} ref={sectionRef} className="w-full py-6 px-4 scroll-mt-16">
